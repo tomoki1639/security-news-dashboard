7a6e7870203f93e67a139c0a32c799b88f93a41e
tomoki1639 <march.9.tomonaka@gmail.com>
2026-04-24 23:43:57 +0900

    Update: main.py

--- main.py
+++ main.py
@@ -14,8 +14,7 @@ if SQLALCHEMY_DATABASE_URL.startswith("postgres://"):
         "postgres://", "postgresql://", 1)
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={
-                       "check_same_thread": False})
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
